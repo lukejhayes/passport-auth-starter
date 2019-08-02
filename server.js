43794fc7a8cad4585c82a06d7c140f631326c369
Luke Hayes <lukehayes2021@u.northwestern.edu>
2019-08-02 14:49:01 -0500

    Passport

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const session      = require('express-session');
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/passportauth");
 
-// require('./config/passport')(passport); // pass passport for configuration
+require('./config/passport')(passport); // pass passport for configuration
 
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
@@ -31,7 +31,7 @@ app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
 // routes ======================================================================
-// require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
 // launch ======================================================================
 app.listen(PORT, () =>{
